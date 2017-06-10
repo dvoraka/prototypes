d8e2964aa23f66334c4470f33613340121915ebc
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-10 16:21:08 +0200

    #3 Prepare build configuration

--- storage-perf/src/main/java/dvoraka/prototype/storage/App.java
+++ storage-perf/src/main/java/dvoraka/prototype/storage/App.java
@@ -1,10 +1,15 @@
 package dvoraka.prototype.storage;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Main app.
  */
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("App");
+        SpringApplication.run(App.class, args);
     }
 }
